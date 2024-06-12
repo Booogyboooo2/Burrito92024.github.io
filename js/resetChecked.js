7a00450d0fda71ea755ef1906195fe8a7435a962
Booogyboooo2 <spamsignup16@gmail.com>
2024-06-12 20:15:00 +0000

    update js

--- js/resetChecked.js
+++ js/resetChecked.js
@@ -1,8 +1,17 @@
-//This file is not used it is just the code of how it works
+//This file is not used it is just the how the code works
 //Instead I used a bookmarklet to retain the same look and not use a button
 
+//Clear Checkmarks
 var i = 0;
 while (i < document.getElementsByTagName("input").length) {
     document.getElementsByTagName("input")[i].checked = false;
+    i++;
+}
+
+//Remove Unused/Duplicate Attributes
+var i = 0;
+while (i < document.getElementsByTagName("input").length) {
+    document.getElementsByTagName("input")[i].removeAttribute('id')
+    document.getElementsByTagName("input")[i].removeAttribute('name')
     i++;
 }
